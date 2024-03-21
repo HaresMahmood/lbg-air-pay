0b68cddc7a0d6f6a864d00d5d6fceacbff2dbc17
Hares Mahmood <hmahmood229@gmail.com>
2024-03-21 15:17:43 +0000

    fix: rename

---
+++ application/src/components/page/account-debit-card.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+type AccountDebitCardProps = {
+  type: string
+  balance: number
+  accountNumber: string
+  sortCode: string
+}
+
+export function AccountDebitCard({
+     type: type, balance, accountNumber, sortCode
+}: AccountDebitCardProps) {  
+  
+    return (
+    <Card className="aspect-[1.586] w-[300px] flex flex-col" >
+      <CardHeader>
+        <CardDescription> {type} </CardDescription>
+        <CardTitle className="text-3xl"> £{balance} </CardTitle>
+        <CardDescription>After pending transactions</CardDescription>
+      </CardHeader>
+      <CardContent>
+        
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <CardDescription> {sortCode} • {accountNumber} </CardDescription>
+      </CardFooter>
+    </Card>
+    )
+}
